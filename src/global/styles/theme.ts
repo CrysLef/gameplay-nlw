f8acf94cb89bd47c10eb08ebdca0bf715d632fe3
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-08 15:55:48 -0300

    Create theme.ts

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+    COLORS: {
+        PRIMARY: '#E51C44',
+
+        SECONDARY_100: '#0A1033',
+        SECONDARY_90: '#0D133E',
+        SECONDARY_80: '#0E1647',
+        SECONDARY_85: '#171F52',
+        SECONDARY_70: '#1B2565',
+        SECONDARY_60: '#1D2766',
+        SECONDARY_50: '#243189',
+        SECONDARY_40: '#495BCC',
+
+
+        OVERLAY: 'rgba(0,0,0,0.7)',
+        HIGHLIGHT: '#ABB1CC',
+        HEADING: '#DDE3F0',
+        LINE: '#991F36',
+        ON: '#32BD50'
+    },
+    FONTS: {
+        TITLE_700: 'Rajdhani_700Bold',
+        TITLE_500: 'Rajdhani_500Medium',
+        TEXT_400: 'Inter_400Regular',
+        TEXT_500: 'Inter_500Medium',
+    },
+}
