bbf67e08169324a358c1e02c87506e14693e59bc
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-09 20:17:46 -0300

    create the CategorySelect component

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from './styles';
+
+import Category from '../Category';
+import { categories } from '../../utils/categories';
+
+type Props = {
+  categorySelected: string,
+  setCategory: (categoryId: string) => void
+}
+
+export default function CategorySelect({ 
+  categorySelected, 
+  setCategory 
+}: Props){
+  return (
+    <Container
+        horizontal
+        showsHorizontalScrollIndicator
+        contentContainerStyle={{ paddingRight: 40 }}
+    >
+        { 
+          categories.map(category => (
+            <Category 
+              key={category.id}
+              title={category.title}
+              icon={category.icon}
+              checked={category.id === categorySelected}
+              onPress={() => setCategory(category.id)}
+            />
+          ))
+        }
+
+    </Container>
+  );
+}
---
+++ src/components/CategorySelect/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.ScrollView`
+   min-height: 120px;
+   max-height: 120px;
+   padding-left: 24px;
+   margin-top: 40px;
+`
