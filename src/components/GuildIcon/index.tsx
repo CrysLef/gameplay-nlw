98826575bd6312b4eb0138009bbbf496d197a2d4
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-09 20:18:02 -0300

    create the GuildIcon component

---
+++ src/components/GuildIcon/index.tsx
@@ -0,0 +1,13 @@
+import { Img } from './styles';
+
+export default function GuildIcon(){
+  const uri = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6ck5k0kovUsCvEoH35qK6L5tmCim2Y-AylA&usqp=CAU'
+
+    return (
+    <Img
+        source={{ uri }}
+        resizeMode='cover'
+    />
+
+  );
+}
