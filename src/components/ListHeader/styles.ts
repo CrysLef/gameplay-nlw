3507b6219930d597ac38a256e60ceb3d0eef4545
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-09 20:20:26 -0300

    create the ListHeader component

---
+++ src/components/ListHeader/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import theme from '../../global/styles/theme';
+
+export const Container = styled.View`
+   width: 100%;
+   flex-direction: row;
+   justify-content: space-between;
+   padding: 0 24px;
+`
+
+export const Title = styled.Text`
+    font-family: ${theme.FONTS.TITLE_700};
+    color: ${theme.COLORS.HEADING};
+    font-size: 18px;
+`
+
+export const Subtitle = styled.Text`
+    font-family: ${theme.FONTS.TEXT_400};
+    color: ${theme.COLORS.HIGHLIGHT};
+    font-size: 13px;
+`
