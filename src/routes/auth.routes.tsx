c60527a3ba3e920f7a74290c838bf6db294fcf81
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-11 17:56:45 -0300

    Add AppointmentDetails route

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,6 +3,7 @@ import theme from "../global/styles/theme"
 
 import Home from '../screens/Home'
 import SignIn from '../screens/SignIn'
+import AppointmentDetails from '../screens/AppointmentDetails'
 
 
 const { Navigator, Screen } = createStackNavigator()
@@ -11,19 +12,25 @@ export default function AuthRoutes() {
     return (
         <Navigator
             screenOptions={{
+                headerShown: false,
                 cardStyle: {
                     backgroundColor: theme.COLORS.SECONDARY_90,
                 },
-                headerShown: false,
             }}
         >
             <Screen 
                 name='SignIn'
                 component={SignIn}
             />
+
             <Screen 
                 name='Home'
                 component={Home}
+            />
+
+             <Screen 
+                name='AppointmentDetails'
+                component={AppointmentDetails}
             />
         </Navigator>
     )
