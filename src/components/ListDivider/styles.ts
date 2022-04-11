6b596354afbed7607a47de8a350b0fc76b1bf381
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-11 17:53:10 -0300

    Fix ListDivider style

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components/native';
 import theme from '../../global/styles/theme';
 
 export const Container = styled.View`
-   width: 80%;
+   width: 78%;
    height: .5px;
    background-color: ${theme.COLORS.SECONDARY_50};
-   margin: 21px 0;
+   margin: 31px 0;
+   margin-top: 2px;
    align-self: flex-end;
 `
