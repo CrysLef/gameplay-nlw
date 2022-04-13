4af143adf40f6c62c0833a6c66f13451424842dc
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-13 20:11:08 -0300

    Refactor CategorySelect component

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -1,38 +1,39 @@
-import { Container } from './styles';
+import { Container } from './styles'
 
-import Category from '../Category';
-import { categories } from '../../utils/categories';
+import Category from '../Category'
+
+import { categories } from '../../utils/categories'
 
 type Props = {
-  categorySelected: string,
-  setCategory: (categoryId: string) => void,
+  categorySelected: string;
   hasCheckBox?: boolean;
+  setCategory: (categoryId: string) => void;
 }
+
 
 export default function CategorySelect({ 
   categorySelected, 
-  setCategory,
-  hasCheckBox = false 
+  hasCheckBox = false, 
+  setCategory
 }: Props){
   return (
     <Container
-        horizontal
-        showsHorizontalScrollIndicator
-        contentContainerStyle={{ paddingRight: 40 }}
+      horizontal
+      showsHorizontalScrollIndicator
+      contentContainerStyle={{ paddingRight: 40 }}
     >
-        { 
-          categories.map(category => (
-            <Category 
-              key={category.id}
-              title={category.title}
-              icon={category.icon}
-              checked={category.id === categorySelected}
-              onPress={() => setCategory(category.id)}
-              hasCheckBox={hasCheckBox}
-            />
-          ))
-        }
-
+      { 
+        categories.map(category => (
+          <Category 
+            key={category.id}
+            title={category.title}
+            icon={category.icon}
+            checked={category.id === categorySelected}
+            onPress={() => setCategory(category.id)}
+            hasCheckBox={hasCheckBox}
+          />
+        ))
+      }
     </Container>
-  );
+  )
 }
