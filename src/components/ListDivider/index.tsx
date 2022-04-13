a4e5ed5aa7021d7fee6038234115bb8e3b10f046
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-13 20:45:11 -0300

    Update ListDivider

--- src/components/ListDivider/index.tsx
+++ src/components/ListDivider/index.tsx
@@ -1,8 +1,23 @@
-import { Container } from './styles';
+import { Container } from './styles'
+import { ViewProps } from 'react-native'
+
+type Props = ViewProps & {
+  isCentered?: boolean;
+}
 
 
-export default function ListDivider(){
+export default function ListDivider({ isCentered, ...rest }: Props){
   return (
-    <Container />
+    <Container 
+      {...rest}
+      style={[
+        isCentered ? {
+          marginVertical: 12,
+        } : {
+          marginTop: 2,
+          marginBottom: 31,
+        }
+      ]}
+    />
   );
 }
--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -1,11 +1,9 @@
-import styled from 'styled-components/native';
-import theme from '../../global/styles/theme';
+import styled from 'styled-components/native'
+import theme from '../../global/styles/theme'
 
 export const Container = styled.View`
-   width: 75%;
+   width: 78%;
    height: .5px;
-   background-color: ${theme.COLORS.SECONDARY_50};
-   margin: 31px 0;
-   margin-top: 2px;
    align-self: flex-end;
+   background-color: ${theme.COLORS.SECONDARY_50};
 `
