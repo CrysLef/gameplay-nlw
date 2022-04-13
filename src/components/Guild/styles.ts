442708768b7b8f1273a01cd453bc4e832742e5ef
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-13 20:12:39 -0300

    Add border to Guild component

--- src/components/Guild/styles.ts
+++ src/components/Guild/styles.ts
@@ -1,5 +1,6 @@
-import styled from 'styled-components/native';
-import theme from '../../global/styles/theme';
+import styled from 'styled-components/native'
+import theme from '../../global/styles/theme'
+
 
 export const Container = styled.TouchableOpacity`
    width: 100%;
@@ -11,18 +12,18 @@ export const Container = styled.TouchableOpacity`
 export const Content = styled.View`
    flex: 1;
    justify-content: center;
+   margin-left: 20px;
 `
 
 export const Title = styled.Text`
    font-family: ${theme.FONTS.TITLE_700};
-   color: ${theme.COLORS.HEADING};
    font-size: 18px;
-   margin-bottom: 11px;
+   color: ${theme.COLORS.HEADING};
+   margin-bottom: 4px;
 `
 
 export const Type = styled.Text`
    font-family: ${theme.FONTS.TEXT_400};
-   color: ${theme.COLORS.HIGHLIGHT};
    font-size: 13px;
-   margin-bottom: 24px;
+   color: ${theme.COLORS.HIGHLIGHT};
 `
