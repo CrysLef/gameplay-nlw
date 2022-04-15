cb6547bbafab709cc135bd0c4ed92fc4f0f1950f
Crystian Lefundes <crystian.lf@gmail.com>
2022-04-15 11:40:44 -0300

    Create discord api configuration

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const CLIENT_ID = '964228725052960828'
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-39231b7b-1119-48b2-be69-9f261664d265'
+const RESPONSE_TYPE = 'token'
+const SCOPE = 'identify%20email%20connections%20guilds'
+const CDN_IMAGE = 'https://cdn.discordapp.com'
+
+export {
+    CLIENT_ID,
+    REDIRECT_URI,
+    RESPONSE_TYPE,
+    SCOPE,
+    CDN_IMAGE
+}
